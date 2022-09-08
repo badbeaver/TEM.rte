124755eb1e5711b002c146309e262584d79a6564
badbeaver <34551546+badbeaver@users.noreply.github.com>
2022-09-08 05:53:07 +0300

    Terra-Era Manufacturing 1.0
    Created TEM.rte, added weapons C96 Mauser, Broomhandle, M712 Mauser Schnellfeuer. Added script that binds switching fire modes when 'C' key is pressed (Bebo is cool).

---
+++ TEM.rte/Devices/Weapons/SmallArms/M712 Mauser Schnellfeuer/SemiAuto.lua
@@ -0,0 +1,20 @@
+function Update(self)
+    self.parent = self:GetRootParent();
+    if self.parent and IsActor(self.parent) then
+        -- Everything under for weapon
+        self.parent = ToActor(self.parent);
+        local ctrl = self.parent:GetController();
+        local playerControlled = self.parent:IsPlayerControlled();
+        if self.parent:IsPlayerControlled() then
+            if UInputMan:KeyPressed(3) then
+				if self.FullAuto == false then
+					self.FullAuto = true
+					self.FireRate = 850
+				elseif self.FullAuto == true then
+					self.FullAuto = false
+					self.FireRate = 500
+				end
+            end
+        end
+    end
+end
